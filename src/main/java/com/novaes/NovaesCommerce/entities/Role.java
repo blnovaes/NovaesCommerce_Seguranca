4d947f6d61db4906c105d5de23f49147846dca02
Bruno Labate Novaes <blnovaes@hotmail.com>
2025-09-25 11:46:38 -0300

    Inclusão do UserDetails e GrantedAutority

--- src/main/java/com/novaes/NovaesCommerce/entities/Role.java
+++ src/main/java/com/novaes/NovaesCommerce/entities/Role.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.Objects;
+import org.springframework.security.core.GrantedAuthority;
 
 /**
  *
@@ -17,7 +18,7 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,6 +41,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
