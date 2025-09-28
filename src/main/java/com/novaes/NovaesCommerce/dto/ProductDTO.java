a8509fcaaae14a4e26982f1eae11dd1ab2dc6858
Bruno Labate Novaes <blnovaes@hotmail.com>
2025-09-28 14:10:43 -0300

    Correção da falidação de produtos

--- src/main/java/com/novaes/NovaesCommerce/dto/ProductDTO.java
+++ src/main/java/com/novaes/NovaesCommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.novaes.NovaesCommerce.entities.Category;
 import com.novaes.NovaesCommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private String description;
     
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     
